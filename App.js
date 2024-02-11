56fb7cacee891b98866571f0e3ce8f0c0020ae6e
Gurkirat Kaur <152523419+kirat13@users.noreply.github.com>
2024-02-12 03:28:53 +0530

    Add files via upload
    Using React JS And Bootstrap

---
+++ App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+} from "react-router-dom";
+export default class App extends Component {
+  pageSize=15;
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+          <Route exact path="/" element={< News  key='general' pageSize={this.pageSize} country="in" category="general"/>}></Route>
+          <Route exact path="/Business" element={ <News key='business'  pageSize={this.pageSize} country="in" category="business"/>}></Route>
+          <Route exact path="/Entertainment" element={<News key='entertainment'  pageSize={this.pageSize} country="in" category="entertainment"/>}></Route>
+          <Route exact path="/General" element={ <News key='general'  pageSize={this.pageSize} country="in" category="general"/>}></Route>
+          <Route exact path="/Health" element={ <News key='health'  pageSize={this.pageSize} country="in" category="health"/>}></Route>
+          <Route exact path="/Science" element={ <News key='science'  pageSize={this.pageSize} country="in" category="science"/>}></Route>
+          <Route exact path="/Sports" element={ <News key='sports'  pageSize={this.pageSize} country="in" category="sports"/>}></Route>
+          <Route exact path="/Technology" element={ <News  key='technology}>' pageSize={this.pageSize} country="in" category="technology"/>}></Route>
+
+
+          </Routes>
+        </Router>
+
+      </div>
+    );
+  }
+}
+
